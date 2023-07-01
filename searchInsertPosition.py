f6609b56b7b4f41b1ef39a75407cb17f4913d125
Robert Biebrich <112362917+RBiebrich@users.noreply.github.com>
2023-07-01 02:15:24 -0400

    Update searchInsertPosition.py

--- searchInsertPosition.py
+++ searchInsertPosition.py
@@ -1,13 +1,22 @@
-#CHALLENGE:
+# CHALLENGE: Search Insert Position
+# DIFFICULTY: Easy
 #===================================================================================
-# Given a sorted array of distinct integers and a target value, return the index if 
-# the target is found. If not, return the index where it would be if it were inserted
-# in order.
+# Given a sorted array of distinct integers and a target value, return the index if
+# the target is found. If not, return the index where it would be if it were
+# inserted in order.
 
 # You must write an algorithm with O(log n) runtime complexity.
 #===================================================================================
 
+#Solution 1:
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+        return len(nums)
 
+#Solution 2:
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         i = 0
