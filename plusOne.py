c704dc6af07fc3480736806928cf6e33e1a39e37
Robert Biebrich <112362917+RBiebrich@users.noreply.github.com>
2023-07-01 02:35:12 -0400

    Update plusOne.py

--- plusOne.py
+++ plusOne.py
@@ -1,23 +1,25 @@
-#CHALLENGE:
+# CHALLENGE: Plus One
+# DIFFICULTY: Easy
 #===================================================================================
 # You are given a large integer represented as an integer array digits, where each
-# digits[i] is the ith digit of the integer. The digits are ordered from most
-# significant to least significant in left-to-right order. The large integer does not
-# contain any leading 0's.
+# digits[i] is the ith digit of the integer. The digits are ordered from most 
+# significant to least significant in left-to-right order. The large integer does
+# not contain any leading 0's.
 
 # Increment the large integer by one and return the resulting array of digits.
 #===================================================================================
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        if set(digits) == {9}:
-            digits.insert(0, 0)
-        for i, digit in list(enumerate(digits))[::-1]:
-            if digit == 9:
+        add = 1
+        for i in reversed(range(len(digits))):
+            if digits[i] == 9:
                 digits[i] = 0
+                if i-1 == -1:
+                    digits.insert(0, 1)
             else:
                 digits[i] += 1
-                break
+                return digits
         return digits
 
 #reverse and find first non-9 digit, work from there
